2ee48da974622a8442ca4f6d0ea902b422ea1401
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-04-12 11:38:34 -0700

    added logic for weird sites

--- index.js
+++ index.js
@@ -3,125 +3,127 @@ import fs from "fs/promises";
 import puppeteer, { Browser, Page } from "puppeteer";
 
 const urls = [
-	 "https://stayawhilevillas.guestybookings.com/properties",
-  "https://openairhomes.guestybookings.com/properties",
-  "https://staywildflower.guestybookings.com/properties",
-  "https://mybookings.guestybookings.com/properties",
-  "https://artthaus.guestybookings.com/properties",
-  "https://abode.guestybookings.com/properties",
-  "https://goodliferesorts.guestybookings.com/properties?city=San+Diego",
-  "https://titanbr.guestybookings.com/properties?city=San+Diego",
-  "https://alter.guestybookings.com/properties",
-  "https://cardo.guestybookings.com/properties",
-  "https://excelsiorstays.guestybookings.com/properties?city=San+Diego&country=United+States",
-  "https://stayrelax.guestybookings.com/properties",
-  "https://skylandvacation.guestybookings.com/properties",
-  "https://amazinghome-presente.guestybookings.com/properties",
-  "https://happiesthouserentals.guestybookings.com/properties",
-  "https://dcfurnishedliving.guestybookings.com/properties",
-  "https://homesweetcity.guestybookings.com/properties",
-  "https://greatdwellings.guestybookings.com/properties",
-  "https://staylocal.guestybookings.com/properties",
-  "https://yolostays.guestybookings.com/properties",
-  "https://valtarealty.guestybookings.com/properties",
-  "https://maverick.guestybookings.com/properties",
-	// "https://luluhomes.guestybookings.com/properties",
-  "https://westhome.guestybookings.com/properties?city=Atlanta",
-  "https://skyhaus.guestybookings.com/properties",
-  "https://woolworthupstairs.guestybookings.com/properties",
-  "https://welcomematvacations.guestybookings.com/properties",
-  "https://aloeyall.guestybookings.com/properties",
-  "https://beckonguests.guestybookings.com/properties",
-  "https://staylocalatx.guestybookings.com/properties",
-  "https://fortunatefoundations.guestybookings.com/properties",
-  "https://thecpcdenver.guestybookings.com/properties",
-  "https://stayinmydistrict.guestybookings.com/properties",
-  "https://thebungalows.guestybookings.com/properties",
-      // "https://booknow.vare.properties/properties",
-  "https://kingstreetcollection.guestybookings.com/properties",
-  "https://thenicholas.guestybookings.com/properties",
-  "https://homefrontstays.guestybookings.com/properties",
-    //  "https://vacationcondosforless.guestybookings.com/properties",
-  "https://oasiscollections.guestybookings.com/properties",
-      // "https://staywithhideaway.guestybookings.com/properties",
-  "https://experiencehedge.guestybookings.com/properties",
-  "https://seagrapehospitality.guestybookings.com/properties",
-  "https://lajollabungalows.guestybookings.com/properties",
-  "https://nxt.guestybookings.com/properties",
-  "https://go_travli.guestybookings.com/properties?city=Sedona&country=United+States",
-  "https://halcyonhomehosting.guestybookings.com/properties",
-  "https://mammothrockvillas.guestybookings.com/properties",
-  "https://unwrittenstays.guestybookings.com/properties",
-  "https://scottsdalebeachclub.guestybookings.com/properties",
-  "https://thectbrothers.guestybookings.com/properties",
-  "https://arizonabnb.guestybookings.com/properties",
-  "https://aspening.guestybookings.com/properties",
-  "https://townandislandcompany.guestybookings.com/properties",
-  "https://brightwild.guestybookings.com/properties",
-  "https://tahoeasap.guestybookings.com/properties",
-  "https://laketahoevacationhomes.guestybookings.com/properties",
-  "https://vistasvacations.guestybookings.com/properties",
-  "https://pvhawaii.guestybookings.com/properties",
-  "https://rsbmanagement.guestybookings.com/properties",
-  "https://myobxvacation.guestybookings.com/properties",
-  "https://vegas.guestybookings.com/properties",
-  "https://mahin.guestybookings.com/properties",
-  "https://lasvegas.guestybookings.com/properties",
-  "https://saltairecottages.guestybookings.com/properties",
-  "https://shsrealtyservices.guestybookings.com/properties",
-  "https://blissfulliving.guestybookings.com/properties",
-  "https://reluxme.guestybookings.com/properties",
-  "https://zionstinygetaway.guestybookings.com/properties",
-  "https://coopmedcenter.guestybookings.com/properties",
-      // "https://seconddoor.guestybookings.com/properties",
-  "https://walkervacationrentals.guestybookings.com/properties",
-  "https://beachhomespuntacana.guestybookings.com/properties",
-  "https://russopg.guestybookings.com/properties",
-  "https://hostedjourney.guestybookings.com/properties",
+	//  "https://stayawhilevillas.guestybookings.com/properties",
+	// "https://openairhomes.guestybookings.com/properties",
+	// "https://staywildflower.guestybookings.com/properties",
+	// "https://mybookings.guestybookings.com/properties",
+	// "https://artthaus.guestybookings.com/properties",
+	// "https://abode.guestybookings.com/properties",
+	// "https://goodliferesorts.guestybookings.com/properties?city=San+Diego",
+	// "https://titanbr.guestybookings.com/properties?city=San+Diego",
+	// "https://alter.guestybookings.com/properties",
+	// "https://cardo.guestybookings.com/properties",
+	// "https://excelsiorstays.guestybookings.com/properties?city=San+Diego&country=United+States",
+	// "https://stayrelax.guestybookings.com/properties",
+	// "https://skylandvacation.guestybookings.com/properties",
+	// "https://amazinghome-presente.guestybookings.com/properties",
+	// "https://happiesthouserentals.guestybookings.com/properties",
+	// "https://dcfurnishedliving.guestybookings.com/properties",
+	// "https://homesweetcity.guestybookings.com/properties",
+	// "https://greatdwellings.guestybookings.com/properties",
+	// "https://staylocal.guestybookings.com/properties",
+	// "https://yolostays.guestybookings.com/properties",
+	// "https://valtarealty.guestybookings.com/properties",
+	// "https://maverick.guestybookings.com/properties",
+	"https://luluhomes.guestybookings.com/properties",
+	// "https://westhome.guestybookings.com/properties?city=Atlanta",
+	// "https://skyhaus.guestybookings.com/properties",
+	// "https://woolworthupstairs.guestybookings.com/properties",
+	// "https://welcomematvacations.guestybookings.com/properties",
+	// "https://aloeyall.guestybookings.com/properties",
+	// "https://beckonguests.guestybookings.com/properties",
+	// "https://staylocalatx.guestybookings.com/properties",
+	// "https://fortunatefoundations.guestybookings.com/properties",
+	// "https://thecpcdenver.guestybookings.com/properties",
+	// "https://stayinmydistrict.guestybookings.com/properties",
+	// "https://thebungalows.guestybookings.com/properties",
+	"https://booknow.vare.properties/properties",
+	// "https://kingstreetcollection.guestybookings.com/properties",
+	// "https://thenicholas.guestybookings.com/properties",
+	// "https://homefrontstays.guestybookings.com/properties",
+	"https://vacationcondosforless.guestybookings.com/properties",
+	// "https://oasiscollections.guestybookings.com/properties",
+	"https://staywithhideaway.guestybookings.com/properties",
+	// "https://experiencehedge.guestybookings.com/properties",
+	// "https://seagrapehospitality.guestybookings.com/properties",
+	// "https://lajollabungalows.guestybookings.com/properties",
+	// "https://nxt.guestybookings.com/properties",
+	// "https://go_travli.guestybookings.com/properties?city=Sedona&country=United+States",
+	// "https://halcyonhomehosting.guestybookings.com/properties",
+	// "https://mammothrockvillas.guestybookings.com/properties",
+	// "https://unwrittenstays.guestybookings.com/properties",
+	// "https://scottsdalebeachclub.guestybookings.com/properties",
+	// "https://thectbrothers.guestybookings.com/properties",
+	// "https://arizonabnb.guestybookings.com/properties",
+	// "https://aspening.guestybookings.com/properties",
+	// "https://townandislandcompany.guestybookings.com/properties",
+	// "https://brightwild.guestybookings.com/properties",
+	// "https://tahoeasap.guestybookings.com/properties",
+	// "https://laketahoevacationhomes.guestybookings.com/properties",
+	// "https://vistasvacations.guestybookings.com/properties",
+	// "https://pvhawaii.guestybookings.com/properties",
+	// "https://rsbmanagement.guestybookings.com/properties",
+	// "https://myobxvacation.guestybookings.com/properties",
+	// "https://vegas.guestybookings.com/properties",
+	// "https://mahin.guestybookings.com/properties",
+	// "https://lasvegas.guestybookings.com/properties",
+	// "https://saltairecottages.guestybookings.com/properties",
+	// "https://shsrealtyservices.guestybookings.com/properties",
+	// "https://blissfulliving.guestybookings.com/properties",
+	// "https://reluxme.guestybookings.com/properties",
+	// "https://zionstinygetaway.guestybookings.com/properties",
+	// "https://coopmedcenter.guestybookings.com/properties",
+	"https://seconddoor.guestybookings.com/properties",
+	// "https://walkervacationrentals.guestybookings.com/properties",
+	// "https://beachhomespuntacana.guestybookings.com/properties",
+	// "https://russopg.guestybookings.com/properties",
+	// "https://hostedjourney.guestybookings.com/properties",
 ];
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-
 //eslint-disable-next-line import/no-anonymous-default-export
 // export default async (req, res) => {
-	// const url = req.query.url as string;
-	async function main() {
-		process.on("unhandledRejection", (reason, promise) => {
-			console.error("Unhandled Rejection at:", promise, "reason:", reason);
-			// You can add additional debugging information here if needed
-		});
-		try {
-			let propertyData = {};
+// const url = req.query.url as string;
+async function main() {
+	process.on("unhandledRejection", (reason, promise) => {
+		console.error("Unhandled Rejection at:", promise, "reason:", reason);
+		// You can add additional debugging information here if needed
+	});
+	try {
+		let propertyData = {};
 
-			for (const url of urls) {
-				const browser = await puppeteer.launch({
-					// headless: false,
-					args: [
-						`--window-size=1920,1080`,
-						"--disable-background-timer-throttling",
-						"--disable-backgrounding-occluded-windows",
-						"--disable-renderer-backgrounding",
-					],
-					defaultViewport: {
-						width: 1920,
-						height: 1080,
-					},
-					protocolTimeout: 4000000,
-				});
-				const page = await browser.newPage();
-				const newData = await scrapeUrl(browser, page, url, propertyData); // Retrieve data from scrapeUrl function
-				propertyData = { ...propertyData, ...newData };
-				await browser.close();
-			}
-
-			await fs.writeFile("./scrape-data-date.json", JSON.stringify(propertyData));
-
-	// 		res.status(200).json({ success: true, data: propertyData });
-		} catch (error) {
-			console.log('scraping failed:', error);
+		for (const url of urls) {
+			const browser = await puppeteer.launch({
+				// headless: false,
+				args: [
+					`--window-size=1920,1080`,
+					"--disable-background-timer-throttling",
+					"--disable-backgrounding-occluded-windows",
+					"--disable-renderer-backgrounding",
+				],
+				defaultViewport: {
+					width: 1920,
+					height: 1080,
+				},
+				protocolTimeout: 4000000,
+			});
+			const page = await browser.newPage();
+			const newData = await scrapeUrl(browser, page, url, propertyData); // Retrieve data from scrapeUrl function
+			propertyData = { ...propertyData, ...newData };
+			await browser.close();
 		}
+
+		await fs.writeFile(
+			"./scrape-data-date-minus.json",
+			JSON.stringify(propertyData)
+		);
+
+		// 		res.status(200).json({ success: true, data: propertyData });
+	} catch (error) {
+		console.log("scraping failed:", error);
 	}
+}
 // };
 
 async function scrapeUrl(browser, page, url, propertyData) {
@@ -137,18 +139,28 @@ async function scrapeUrl(browser, page, url, propertyData) {
 		while (previousScrollHeight !== currentScrollHeight) {
 			previousScrollHeight = currentScrollHeight;
 
-			await page.evaluate(async () => {
-				const element = document.getElementsByClassName("overflow-y-scroll")[0];
+			currentScrollHeight = await page.evaluate(async () => {
+				let element;
+
+				const overflowYScrollElements =
+					document.getElementsByClassName("overflow-y-scroll");
+
+				if (overflowYScrollElements.length > 0) {
+					element = overflowYScrollElements[0];
+				} else {
+					element = document.documentElement;
+				}
 				if (element) {
 					element.scrollTo(0, element.scrollHeight);
 				}
-			});
+				await new Promise((resolve) => setTimeout(resolve, 1000));
 
-			await new Promise((resolve) => setTimeout(resolve, 2000));
-			currentScrollHeight = await page.evaluate(() => {
-				const element = document.getElementsByClassName("overflow-y-scroll")[0];
 				return element ? element.scrollHeight : -1;
 			});
+
+			// currentScrollHeight = await page.evaluate(() => {
+			// 	const element = document.getElementsByClassName("overflow-y-scroll")[0];
+			// });
 		}
 	}
 
@@ -275,7 +287,10 @@ async function scrapeUrl(browser, page, url, propertyData) {
 
 	try {
 		const propertiesData = await page.evaluate(async () => {
-			const element = document.querySelector(".overflow-y-scroll");
+			let element = document.querySelector(".overflow-y-scroll");
+			if (!element) {
+				element = document.querySelector('div[data-property-list="true"]');
+			}
 			const data = {};
 			if (element) {
 				const divs = Array.from(element.children);
@@ -306,10 +321,7 @@ async function scrapeUrl(browser, page, url, propertyData) {
 							: null,
 						originalNightlyPrice:
 							parseFloat(
-								propertyFooterSpan.textContent
-									.replace(/,/g, "")
-									.trim()
-									.slice(1)
+								propertyFooterSpan.textContent.replace(/,/g, "").trim().slice(1)
 							) * 100,
 					};
 					if (propertyTitle) {
@@ -326,7 +338,10 @@ async function scrapeUrl(browser, page, url, propertyData) {
 
 	try {
 		await page.evaluate(async () => {
-			const element = document.querySelector(".overflow-y-scroll");
+			let element = document.querySelector(".overflow-y-scroll");
+			if (!element) {
+				element = document.querySelector('div[data-property-list="true"]');
+			}
 			if (element) {
 				const divs = Array.from(element.children);
 				// console.log("count", divs.length);
@@ -368,7 +383,7 @@ async function scrapeUrl(browser, page, url, propertyData) {
 	} catch (err) {
 		console.log("err after the page.eval", err);
 	}
-  return tempData;
+	return tempData;
 }
 
 main();
